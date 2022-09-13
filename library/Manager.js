4dece935379675ae0886a46edc9f0ad8aad84e7e
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-12 22:03:40 -0400

    Update Manager.js

--- library/Manager.js
+++ library/Manager.js
@@ -1,1 +1,19 @@
+//Creation of Manager class extended from the employee class
+//Creation of constructor that includes name, id, email, and office number
+//getName, getId, getEmail, getRole function
 
+const Employee = require('./Employee');
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber) {
+        Employee (name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNumber() {
+        return this.getOfficeNumber
+    }
+};
+
+module.exports = Manager;
